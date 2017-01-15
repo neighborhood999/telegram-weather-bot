2977a98db76d172562f9964e5659195579bbe6e7
Peng-Jie <dean.leehom@gmail.com>
2017-01-15 22:31:32 +0800

    mock api test

--- test/api/getWeather.spec.js
+++ test/api/getWeather.spec.js
@@ -1,10 +1,13 @@
 import test from 'ava';
+import mockData from './mock.json';
 import getWeather from '../../src/api/getWeather';
 
 test('should get weather information', async t => {
   const expectedCity = 'Taichung City';
-  const result = await getWeather('Taichung');
+  let mock = await getWeather('Taichung');
+  const { channel } = mockData['query']['results'];
 
-  t.is(typeof result, 'object');
-  t.is(result.location.city, expectedCity);
+  mock = channel; // For mock api response
+
+  t.is(mock.location.city, expectedCity);
 });
