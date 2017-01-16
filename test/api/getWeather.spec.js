b3c0400cfac147f660f325085935fa2b73f73b73
Peng-Jie <dean.leehom@gmail.com>
2017-01-16 14:31:23 +0800

    Add api failure test

--- test/api/getWeather.spec.js
+++ test/api/getWeather.spec.js
@@ -1,13 +1,22 @@
 import test from 'ava';
-import mockData from './mock.json';
+import mockSuccess from './mock.json';
+import mockFailure from './mockFailure.json';
 import getWeather from '../../src/api/getWeather';
 
 test('should get weather information', async t => {
   const expectedCity = 'Taichung City';
   let mock = await getWeather('Taichung');
-  const { channel } = mockData['query']['results'];
+  const { channel } = mockSuccess['query']['results'];
 
   mock = channel; // For mock api response
 
   t.is(mock.location.city, expectedCity);
+});
+
+test('should get weather weather failure information', async t => {
+  let mock = await getWeather('');
+  mock = mockFailure;
+
+  t.is(mock['error']['status'], '400');
+  t.is(mock['error']['description'], 'Empty text');
 });
--- test/utils/weatherCode.spec.js
+++ test/utils/weatherCode.spec.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import weatherEmoji from '../../src/utils/weatherCode';
 
 test('should get weather emoji', t => {
-  const expected = { text:'sunny', icon: '☀️' };
+  const expected = { text: 'sunny', icon: '☀️' };
   const emoji = weatherEmoji(32);
 
   t.is(emoji, expected.icon);
